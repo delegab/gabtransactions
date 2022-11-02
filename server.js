3d91b73fb10fd86a4a6fdf3962304bf3a625f9d5
delegab <115646537+delegab@users.noreply.github.com>
2022-11-02 16:48:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const client = new Client({
   connectionString: "postgres://bvtcewjuoucpei:ff65b3a7d5ff67c1a74cc57cdf0cbfb5ede16358394efbfdbed3de829d892b8d@ec2-52-207-90-231.compute-1.amazonaws.com:5432/d5gchmvrhumllp",
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
 
 client.connect();	
